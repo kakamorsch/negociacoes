f2f667e5f881ba8f104865a76503b2331d2e81ee
kakamorsch <kakamorsch@gmail.com>
2019-04-12 17:49:24 -0300

    Implemtancao de factory para diminuir a verbosidad

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,29 +5,25 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        /*
-        this._listaNegociacoes = new ListaNegociacoes(model=>this._negociacoesView.update(model)); //instacio a classe listaNegociacoes passando como parametro a funcao que pega a view de negociacoes e atualiza
-        */
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
 
-        this._mensagem = new Mensagem();
-        this._mensagemView= new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(), new NegociacoesView ($('#negociacoesView')), 'adiciona','esvazia');
+
+
+
+        this._mensagem = new Bind(new Mensagem(), new MensagemView($('#mensagemView')),'texto');
     }
     //
     adiciona(event){
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto= 'Negociacao adicionada com sucesso!'
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
     //esvazia o array de negociacoes, atualiza a view e atualiza a view das mensagens
     apaga(){
         this._listaNegociacoes.esvazia();
         this._mensagem.texto= 'Negociacoes apagadas com sucesso'
-        this._mensagemView.update(this._mensagem);
     }
     //cria uma negociacao
     _criaNegociacao(){
